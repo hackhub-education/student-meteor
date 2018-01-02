84a3e68b2c6ea2c456501f333e671b03de452949
Michael Chen <cyjdyj@gmail.com>
2018-01-02 14:16:46 -0800

    starter code

--- client/main.js
+++ client/main.js
@@ -1,22 +1,23 @@
-import { Template } from 'meteor/templating';
-import { ReactiveVar } from 'meteor/reactive-var';
+Template.students.onCreated(function() {
 
-import './main.html';
-
-Template.hello.onCreated(function helloOnCreated() {
-  // counter starts at 0
-  this.counter = new ReactiveVar(0);
 });
 
-Template.hello.helpers({
-  counter() {
-    return Template.instance().counter.get();
+Template.students.helpers({
+  students() {
+    return [{
+      firstname: 'Michael',
+      lastname: 'Chen',
+      email: 'michael@hackhub.com',
+      age: 25
+    }, {
+      firstname: 'Yan',
+      lastname: 'Hong',
+      email: 'yan@hackhub.com',
+      age: 25
+    }];
   },
 });
 
-Template.hello.events({
-  'click button'(event, instance) {
-    // increment the counter when button is clicked
-    instance.counter.set(instance.counter.get() + 1);
-  },
+Template.students.events({
+
 });
